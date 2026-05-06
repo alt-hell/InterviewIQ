a2f78cca4328f4fcd123b7575d4b3a273d0101be
alt-hell <sohail14581@gmail.com>
2026-05-06 14:12:33 +0530

    Fix production cookies and multer upload path

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,8 +1,14 @@
 import multer from "multer";
 
+import fs from "fs";
+
 const storage = multer.diskStorage({
     destination: function(req, file , cb){
-        cb(null , "public")
+        const dir = "public";
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null , dir)
     },
     filename: function(req , file , cb){
         const filename = Date.now() + "-" + file.originalname;
