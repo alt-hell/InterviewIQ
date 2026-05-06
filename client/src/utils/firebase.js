6800424a44702a675115f9147ed5dd2e63295515
alt-hell <sohail14581@gmail.com>
2026-05-06 13:52:55 +0530

    Update firebase config to new project

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890abcdef"
+  authDomain: "interviewiq-14708.firebaseapp.com",
+  projectId: "interviewiq-14708",
+  storageBucket: "interviewiq-14708.firebasestorage.app",
+  messagingSenderId: "842132163948",
+  appId: "1:842132163948:web:3d11ecfa28f65096653749",
+  measurementId: "G-VQMFN81E4Q"
 };
 
 const app = initializeApp(firebaseConfig);
